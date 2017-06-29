73a1720806472a66f4c394545dd87fd7542066e1
UAEDF-tomc <tom.cornelis@ua.ac.be>
2017-06-29 18:22:41 +0200

    add copyIndexPHP function

--- tools/python/helpers.py
+++ tools/python/helpers.py
@@ -1,4 +1,4 @@
-import ROOT
+import ROOT, os
 
 #
 # Get object (e.g. hist) from file using key
@@ -13,3 +13,18 @@ def getObjFromFile(fname, hname):
     res = htmp.Clone()
     f.Close()
     return res
+
+#
+# Copy the index.php file to plotting directory
+#
+def copyIndexPHP(directory):
+  index_php = os.path.join(directory, 'index.php' )
+  if not os.path.exists(directory): os.makedirs(directory)
+  if not directory[-1] == '/': directory = directory+'/'
+  subdirs = directory.split('/')
+  for i in range(1,len(subdirs)):
+    p = '/'.join(subdirs[:-i])
+    index_php = os.path.join(p, 'index.php')
+    shutil.copyfile(os.path.expandvars( '$CMSSW_BASE/src/ttg/tools/php/index.php'), index_php)
+
+
