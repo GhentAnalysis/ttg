2e5dd7777e88d3da33fe9df3de2a3dc4fa01e2b9
UAEDF-tomc <tom.cornelis@ua.ac.be>
2018-01-08 10:37:25 +0100

    new photon match categorization

--- plots/python/photonCategories.py
+++ plots/python/photonCategories.py
@@ -4,45 +4,57 @@
 
 from ttg.reduceTuple.objectSelection import deltaR
 
-def isSignalPhoton(tree, index):
-  mcIndex = tree._phMatchMCPhotonAN15165[index]
-  if mcIndex < 0:                                                                                               return False
-  if not tree._gen_phPassParentage[mcIndex]:                                                                    return False
-  if (tree._gen_phPt[mcIndex] - tree._phPt[index])/tree._gen_phPt[mcIndex] > 0.1:                               return False
-  if tree._gen_phMinDeltaR[mcIndex] < 0.2:                                                                      return False
-  if deltaR(tree._gen_phEta[mcIndex], tree._phEta[index], tree._gen_phPhi[mcIndex], tree._phPhi[index]) > 0.01: return False
-  if (tree._gen_phEta[mcIndex] - tree._phEta[index])/tree._gen_phEta[mcIndex] > 0.005:                          return False
-  return True
+def isSignalPhoton(tree, index, oldDefinition=False):
+  if not oldDefinition:
+    return tree._phMatchCategoryTTG[index]==1
+  else:
+    mcIndex = tree._phMatchMCPhotonAN15165[index]
+    if mcIndex < 0:                                                                                               return False
+    if not tree._gen_phPassParentage[mcIndex]:                                                                    return False
+    if (tree._gen_phPt[mcIndex] - tree._phPt[index])/tree._gen_phPt[mcIndex] > 0.1:                               return False
+    if tree._gen_phMinDeltaR[mcIndex] < 0.2:                                                                      return False
+    if deltaR(tree._gen_phEta[mcIndex], tree._phEta[index], tree._gen_phPhi[mcIndex], tree._phPhi[index]) > 0.01: return False
+    if (tree._gen_phEta[mcIndex] - tree._phEta[index])/tree._gen_phEta[mcIndex] > 0.005:                          return False
+    return True
 
-def isHadronicPhoton(tree, index):
-  mcIndex = tree._phMatchMCPhotonAN15165[index]
-  if mcIndex < -1:                return False
-  if isSignalPhoton(tree, index): return False
-  return True
+def isHadronicPhoton(tree, index, oldDefinition=False):
+  if not oldDefinition:
+    return tree._phMatchCategoryTTG[index]==3
+  else:
+    mcIndex = tree._phMatchMCPhotonAN15165[index]
+    if mcIndex < -1:                return False
+    if isSignalPhoton(tree, index): return False
+    return True
 
-def isGoodElectron(tree, index):
-  if isSignalPhoton(tree, index):   return False
-  if isHadronicPhoton(tree, index): return False
-  mcIndex = tree._phMatchMCLeptonAN15165[index]
-  if mcIndex < 0:                                                                                             return False
-  if (tree._gen_lPt[mcIndex] - tree._phPt[index])/tree._gen_lPt[mcIndex] > 0.1:                               return False
-# if not tree._gen_lPassParentage[mcIndex]                                                                    return False # To be added to tuples
-# if tree._gen_lMinDeltaR[mcIndex] < 0.2:                                                                     return False # To be added to tuples
-  if deltaR(tree._gen_lEta[mcIndex], tree._phEta[index], tree._gen_lPhi[mcIndex], tree._phPhi[index]) > 0.04: return False
-  if (tree._gen_lEta[mcIndex] - tree._phEta[index])/tree._gen_lEta[mcIndex] > 0.005:                          return False
-  return True;
+def isGoodElectron(tree, index, oldDefinition=False):
+  if not oldDefinition:
+    return tree._phMatchCategoryTTG[index]==2
+  else:
+    if isSignalPhoton(tree, index):   return False
+    if isHadronicPhoton(tree, index): return False
+    mcIndex = tree._phMatchMCLeptonAN15165[index]
+    if mcIndex < 0:                                                                                             return False
+    if (tree._gen_lPt[mcIndex] - tree._phPt[index])/tree._gen_lPt[mcIndex] > 0.1:                               return False
+    if not tree._gen_lPassParentage[mcIndex]:                                                                   return False
+    if tree._gen_lMinDeltaR[mcIndex] < 0.2:                                                                     return False
+    if deltaR(tree._gen_lEta[mcIndex], tree._phEta[index], tree._gen_lPhi[mcIndex], tree._phPhi[index]) > 0.04: return False
+    if (tree._gen_lEta[mcIndex] - tree._phEta[index])/tree._gen_lEta[mcIndex] > 0.005:                          return False
+    return True;
 
-def isHadronicFake(tree, index):
-  if isSignalPhoton(tree, index):   return False
-  if isHadronicPhoton(tree, index): return False
-  if isGoodElectron(tree, index):   return False
-  return True
+def isHadronicFake(tree, index, oldDefinition=False):
+  if not oldDefinition:
+    return tree._phMatchCategoryTTG[index]==4
+  else:
+    if isSignalPhoton(tree, index):   return False
+    if isHadronicPhoton(tree, index): return False
+    if isGoodElectron(tree, index):   return False
+    return True
 
-def checkMatch(tree, index):
-  if tree.genuine        and not isSignalPhoton(tree, index):   return False
-  if tree.hadronicPhoton and not isHadronicPhoton(tree, index): return False
-  if tree.misIdEle       and not isGoodElectron(tree, index):   return False
-  if tree.hadronicFake   and not isHadronicFake(tree, index):   return False
+def checkMatch(tree, index, oldDefinition=False):
+  if tree.genuine        and not isSignalPhoton(tree, index, oldDefinition):   return False
+  if tree.hadronicPhoton and not isHadronicPhoton(tree, index, oldDefinition): return False
+  if tree.misIdEle       and not isGoodElectron(tree, index, oldDefinition):   return False
+  if tree.hadronicFake   and not isHadronicFake(tree, index, oldDefinition):   return False
   return True
 
 def checkPrompt(tree, index):
