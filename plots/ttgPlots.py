f407ae273e8b14aab1eda6070571526ced4780e9
UAEDF-tomc <tom.cornelis@ua.ac.be>
2018-01-18 17:03:36 +0100

    do not include the emu/SF split in the signal region

--- plots/ttgPlots.py
+++ plots/ttgPlots.py
@@ -153,12 +153,12 @@ def genPhotonMinDeltaR(c):
 
 def createSignalRegions(c):
   if c.njets == 1:
-    if c.ndbjets == 0: return (0 if not c.isEMu else 5)
-    if c.ndbjets == 1: return (1 if not c.isEMu else 6)
+    if c.ndbjets == 0: return 0
+    if c.ndbjets == 1: return 1
   elif c.njets >= 2:
-    if c.ndbjets == 0: return (2 if not c.isEMu else 7)
-    if c.ndbjets == 1: return (3 if not c.isEMu else 8)
-    if c.ndbjets >= 2: return (4 if not c.isEMu else 9)
+    if c.ndbjets == 0: return 2
+    if c.ndbjets == 1: return 3
+    if c.ndbjets >= 2: return 4
   return -1
 
 
@@ -223,7 +223,7 @@ else:
   plots.append(Plot('j2_phi',                     '#phi(j_{2})',                          lambda c : c._jetPhi[c.j2],                                    (10,-pi,pi)))
   plots.append(Plot('j2_csvV2',                   'CSVv2(j_{2})',                         lambda c : c._jetCsvV2[c.j2],                                  (20, 0, 1)))
   plots.append(Plot('j2_deepCSV',                 'deepCSV(j_{2})',                       lambda c : c._jetDeepCsv_b[c.j2] + c._jetDeepCsv_bb[c.j2],     (20, 0, 1)))
-  plots.append(Plot('signalRegions',              'signal region',                        lambda c : createSignalRegions(c),                             (10, 0, 10), histModifications=xAxisLabels(['1j,0b', '1j,1b','#geq2j,0b','#geq2j,1b','#geq2j,#geq2b','1j,0b', '1j,1b','#geq2j,0b','#geq2j,1b','#geq2j,#geq2b'])))
+  plots.append(Plot('signalRegions',              'signal region',                        lambda c : createSignalRegions(c),                             (10, 0, 10), histModifications=xAxisLabels(['1j,0b', '1j,1b','#geq2j,0b','#geq2j,1b','#geq2j,#geq2b'])))
   plots.append(Plot('eventType',                  'eventType',                            lambda c : ord(c._ttgEventType),                               (15, 0, 15)))
   plots.append(Plot('genPhoton_pt',               'p_{T}(gen #gamma) (GeV)',              lambda c : c._phTTGMatchPt[c.ph],                              (10,10,110)))
   plots.append(Plot('genPhoton_eta',              '|#eta|(gen #gamma)',                   lambda c : abs(c._phTTGMatchEta[c.ph]),                        (15,0,2.5), overflowBin=None))
