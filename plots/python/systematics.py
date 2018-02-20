dd2d0a8351fd89013a7d6bb4d7856bd2cf63d07a
UAEDF-tomc <tom.cornelis@ua.ac.be>
2018-02-20 10:47:23 +0100

    Adding the systematics

--- plots/python/systematics.py
+++ plots/python/systematics.py
@@ -5,12 +5,18 @@ systematics = {}
 for i in ('Up', 'Down'):
   systematics['isr'+i]      = []
   systematics['fsr'+i]      = []
+  systematics['eScale'+i]   = []
+  systematics['eRes'+i]     = []
+  systematics['phScale'+i]  = []
+  systematics['phRes'+i]    = []
   systematics['pu'+i]       = [('puWeight',      'puWeight'+i)]
   systematics['phSF'+i]     = [('phWeight',      'phWeight'+i)]
   systematics['lSF'+i]      = [('lWeight',       'lWeight'+i)]
   systematics['trigger'+i]  = [('triggerWeight', 'triggerWeight'+i)]
   systematics['bTagl'+i]    = [('bTagWeight',    'bTagWeightl'+i)]
   systematics['bTagb'+i]    = [('bTagWeight',    'bTagWeightb'+i)]
+  systematics['pdf'+i]      = [('genWeight',     'weight_pdf'+i)]
+  systematics['q2'+i]       = [('genWeight',     'weight_q2'+i)]
   systematics['JEC'+i]      = [('njets',         'njets_JEC'+i),     ('ndbjets',    'ndbjets_JEC'+i), ('j1', 'j1_JEC'+i), ('j2', 'j2_JEC'+i), ('_jetPt', '_jetPt_JEC'+i)]
   systematics['JER'+i]      = [('njets',         'njets_JER'+i),     ('ndbjets',    'ndbjets_JER'+i), ('j1', 'j1_JER'+i), ('j2', 'j2_JER'+i), ('_jetPt', '_jetPt_JER'+i)]
 
