937b451504e40d08074b1200e8faa979fb661433
UAEDF-tomc <tom.cornelis@ua.ac.be>
2018-03-14 13:50:52 +0100

    Remove the old fit script

--- plots/createFit.py
+++ plots/createFit.py
@@ -2,148 +2,147 @@
 import argparse
 argParser = argparse.ArgumentParser(description = "Argument parser")
 argParser.add_argument('--logLevel',       action='store',      default='INFO',      nargs='?', choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'TRACE'], help="Log level for logging")
-argParser.add_argument('--checkStability', action='store_true', default=False)
 args = argParser.parse_args()
 
-from ttg.plots.plot         import Plot, getHistFromPkl
 from ttg.tools.logger       import getLogger
-from ttg.plots.combineTools import handleCombine
-log = getLogger(args.logLevel)
+log = getLogger(logLevel=args.logLevel)
+
+from ttg.plots.plot         import getHistFromPkl, normalizeBinWidth
+from ttg.plots.combineTools import writeCard, runFitDiagnostics, runSignificance, runImpacts
+from ttg.plots.systematics  import systematics, linearSystematics
 
 import os, ROOT, shutil
 ROOT.gROOT.SetBatch(True)
 
+from math import sqrt
 
-def writeStatVariation(hist, prefix):
-  for i in range(hist.GetNbinsX()):
-    bin  = i+1
-    up   = hist.Clone()
-    down = hist.Clone()
-    up.SetBinContent(  bin, hist.GetBinContent(bin)+hist.GetBinError(bin))
-    down.SetBinContent(bin, hist.GetBinContent(bin)-hist.GetBinError(bin))
-    up.Write(prefix + str(i) + 'Up')
-    down.Write(prefix + str(i) + 'Down')
+samples     = ['TTGamma','TTJets','ZG','DY','other']
 
-for channel in ['ee', 'emu', 'mumu']:
-  mcPurities = []
-  for pseudoData in [True, False]:
-    for j in (range(-5, 20, 1) if pseudoData and args.checkStability else [0]):
-      log.info('Creating templates and card for ' + channel)
-      dataCard = 'purityFit_' + channel + ('_pseudoData' if pseudoData else '')
-      selection = 'llg-looseLeptonVeto-mll40-offZ-llgNoZ-njet2p-deepbtag1p'
-      randomConeSelection    = getHistFromPkl(('eleSusyLoose-phoCBnoChgIso', channel, selection), 'photon_randomConeIso', ['data'])
-      sigmaIetaIetaSideBand  = getHistFromPkl(('sigmaIetaIetaMatchMC',       channel, selection), 'photon_chargedIso',    ['data'])
-      if pseudoData:
-        chargedIsoTTGamma    = getHistFromPkl(('eleSusyLoose-phoCBnoChgIso', channel, selection), 'photon_chargedIso',    ['t#bar{t}#gamma'])
-        chargedIsoOther      = getHistFromPkl(('eleSusyLoose-phoCBnoChgIso', channel, selection), 'photon_chargedIso',    ['TTJets'])
-        chargedIsoOther.Add(   getHistFromPkl(('eleSusyLoose-phoCBnoChgIso', channel, selection), 'photon_chargedIso',    ['Z#gamma']))
-        chargedIsoOther.Add(   getHistFromPkl(('eleSusyLoose-phoCBnoChgIso', channel, selection), 'photon_chargedIso',    ['Drell-Yan']))
-        chargedIsoOther.Add(   getHistFromPkl(('eleSusyLoose-phoCBnoChgIso', channel, selection), 'photon_chargedIso',    ['single-t']))
-        chargedIsoOther.Add(   getHistFromPkl(('eleSusyLoose-phoCBnoChgIso', channel, selection), 'photon_chargedIso',    ['multiboson']))
-        chargedIsoOther.Add(   getHistFromPkl(('eleSusyLoose-phoCBnoChgIso', channel, selection), 'photon_chargedIso',    ['t#bar{t}+V']))
-        chargedIsoTTGamma.Scale(1+j*0.1)
-        chargedIsoData = chargedIsoTTGamma.Clone()
-        chargedIsoData.Add(chargedIsoOther)
-        mcResult = chargedIsoTTGamma.Integral()/chargedIsoData.Integral()
-        log.info('Expected fit for mc: %.3f' % mcResult)
-      else:
-        chargedIsoData      = getHistFromPkl(('eleSusyLoose-phoCBnoChgIso',  channel, selection), 'photon_chargedIso',    ['data'])
-      randomConeSelection.Scale(chargedIsoData.Integral()/randomConeSelection.Integral())
-      sigmaIetaIetaSideBand.Scale(chargedIsoData.Integral()/sigmaIetaIetaSideBand.Integral())
+def protectHist(hist):
+  if hist.Integral() < 0.00001:
+    for i in range(1, hist.GetNbinsX()+1):
+      if hist.GetBinContent(i) <= 0.00001: hist.SetBinContent(i, 0.00001)
+  return hist
 
-      rootfile = ROOT.TFile(dataCard + '.root', 'RECREATE')
-      randomConeSelection.Write('signal')
-      sigmaIetaIetaSideBand.Write('background')
-      writeStatVariation(randomConeSelection,   'signal_statSig')
-      writeStatVariation(sigmaIetaIetaSideBand, 'background_statBack')
-      chargedIsoData.Write('data_obs')
+def removeFirstBin(hist):
+  hist.SetBinContent(1, 0)
+  hist.SetBinError(1, 0)
+  return hist
 
-      with open('combineCard.txt', 'r') as source:
-        with open(dataCard + '.txt', 'w') as destination:
-          for line in source:
-            line = line.replace('$FILE',        dataCard + '.root')
-            destination.write(line)
-          for i in range(chargedIsoData.GetNbinsX()):
-            continue # adding statistics does not work
-            if randomConeSelection.GetBinContent(i+1):   destination.write('statSig' + str(i) + '  shapeN2      1          -\n')
-            if sigmaIetaIetaSideBand.GetBinContent(i+1): destination.write('statBack' + str(i) + ' shapeN2      -          1\n')
-      rootfile.Close()
+def applyNonPromptSF(hist, nonPromptSF):
+  hist.Scale(nonPromptSF['njet2p-deepbtag1p'][0])  # Currently scaling all SR with same factor from njet2p-deepbtag1p, could be adapted to specific SF for each SR
+  return hist
 
-      result = handleCombine(dataCard)
+def writeHist(file, shape, template, hist, statVariations=None):
+  if not file.GetDirectory(shape): file.mkdir(shape)
+  file.cd(shape)
+  protectHist(hist).Write(template)
+  file.cd()
+  if statVariations is not None:
+    for i in range(1, hist.GetNbinsX()+1):
+      up   = hist.Clone()
+      down = hist.Clone()
+      up.SetBinContent(  i, hist.GetBinContent(i)+hist.GetBinError(i))
+      down.SetBinContent(i, hist.GetBinContent(i)-hist.GetBinError(i))
+      writeHist(file, shape + shape + template + 'Stat' + str(i) + 'Up',   template, up)
+      writeHist(file, shape + shape + template + 'Stat' + str(i) + 'Down', template, down)
+      statVariations.append(shape + template + 'Stat' + str(i))
 
-      import ttg.tools.style as styles
-      chargedIsoData.style        = styles.errorStyle(ROOT.kBlack)
-      randomConeSelection.style   = styles.fillStyle(ROOT.kRed)
-      sigmaIetaIetaSideBand.style = styles.fillStyle(ROOT.kBlue)
+#
+# ROOT File for a charged isolation only fit
+#
+def writeRootFileForChgIso(name, systematics, selection):
+  try:    os.makedirs('combine')
+  except: pass
 
-      randomConeSelection2   = randomConeSelection.Clone()
-      sigmaIetaIetaSideBand2 = sigmaIetaIetaSideBand.Clone()
-      randomConeSelection2.style   = styles.fillStyle(ROOT.kRed)
-      sigmaIetaIetaSideBand2.style = styles.fillStyle(ROOT.kBlue)
-      randomConeSelection2.Scale(result[0])
-      sigmaIetaIetaSideBand2.Scale(1-result[0])
+  selection = 'llg-looseLeptonVeto-mll40-offZ-llgNoZ-' + selection + '-photonPt20'
 
-      chargedIsoData.texName = 'data'
-      randomConeSelection.texName = 'signal (prefit)'
-      sigmaIetaIetaSideBand.texName = 'background (prefit)'
-      randomConeSelection2.texName = 'signal (after fit)'
-      sigmaIetaIetaSideBand2.texName = 'background (after fit)'
+  f = ROOT.TFile('combine/' + name + '.root', 'RECREATE')
+
+  plot = 'photon_chargedIso_bins_NO'
+  writeHist(f, 'chgIso' , 'data_obs', normalizeBinWidth(removeFirstBin(getHistFromPkl(('eleSusyLoose-phoCBnoChgIso-match', 'all', selection), plot, '', ['MuonEG'],['DoubleEG'],['DoubleMuon'])),norm=1))
+
+  statVariations = []
+  for splitType in ['_p', '_np']:
+    for sys in [''] + systematics:
+      if   splitType=='_p':  selectors = [[sample, '(genuine,misIdEle)'] for sample in samples]
+      elif splitType=='_np': selectors = [[sample, '(hadronicPhoton,hadronicFake)'] for sample in samples]
+      writeHist(f, 'chgIso'+sys,  'all' + splitType, normalizeBinWidth(removeFirstBin(getHistFromPkl(('eleSusyLoose-phoCBnoChgIso-match', 'all', selection), plot, sys, *selectors)), norm=1), (statVariations if sys=='' else None))
+
+  return set(statVariations)
+  f.Close()
 
 
-      prefit        = Plot('prefit', 'chargedIso(#gamma) (GeV)', None, None, overflowBin=None, stack=[[]], texY='Events')
-      prefit.stack  = [[randomConeSelection,sigmaIetaIetaSideBand],[chargedIsoData]]
-      prefit.histos = {i:i for i in sum(prefit.stack, [])}
+#
+# Charged isolation fit
+#
+templates   = ['all_np', 'all_p'] 
+extraLines  = ['prompt_norm rateParam * all_p 1']
 
-      fit        = Plot('fit', 'chargedIso(#gamma) (GeV)', None, None, overflowBin=None, stack=[[]], texY='Events')
-      fit.stack  = [[randomConeSelection2,sigmaIetaIetaSideBand2],[chargedIsoData]]
-      fit.histos = {i:i for i in sum(fit.stack, [])}
+nonPromptSF = {}
+#for selection in ['njet1-deepbtag0', 'njet1-deepbtag1p', 'njet2p-deepbtag0', 'njet2p-deepbtag1', 'njet2p-deepbtag2p','njet2p-deepbtag1p']:
+for selection in ['njet2p-deepbtag1p']:
+  cardName = 'chgIsoFit_' + selection
+  statVariations = writeRootFileForChgIso(cardName, systematics.keys(), selection)
+  writeCard(cardName, ['chgIso'], templates, extraLines, systematics.keys(), statVariations, linearSystematics)
+  systematics = {}
+  result = runFitDiagnostics(cardName, toys=None, statOnly=False)
+  nonPromptSF[selection] = (result[0], sqrt((result[1]/result[0])**2+0.25**2)*result[0], -sqrt((result[2]/result[0])**2+0.25**2)*result[0])    # Add extra uncertainty of 25% based on different chgIso shape in sigmaIetaIeta sideband
+  runImpacts(cardName)
 
-      if j==0:
-        from ttg.tools.style import drawLumi
-        for plot in [fit, prefit]:
-          plot.draw(plot_directory = './fitPlots/' + channel + ('_pseudoData' if pseudoData else ''),
-                   ratio   = {'yRange':(0.1,1.9),'texY': 'ratio'},
-                   logX    = False, logY = False, sorting = False,
-                   yRange  = (0.0001, "auto"),
-                   drawObjects = drawLumi(None, 35.9),
-          )
-
-      if pseudoData:
-        mcPurities.append((mcResult, result))
+for i,j in nonPromptSF.iteritems():
+  log.info('Charged isolation fit for ' + i + ' results in %.2f (+%.2f, %.2f)' % j)
 
 
-      def purity(bin, sig, back):
-        return randomConeSelection2.Integral(0, bin)*sig/(randomConeSelection2.Integral(0, bin)*sig+sigmaIetaIetaSideBand2.Integral(0, bin)*back)
 
-      purity5GeV = purity(5, 1, 1)
-      purity1GeV = purity(1, 1, 1)
 
-      signalUp       = (result[0]+result[1])/result[0]
-      backgroundUp   = (1-(result[0]+result[1]))/(1-result[0])
-      signalDown     = (result[0]+result[2])/result[0]
-      backgroundDown = (1-(result[0]+result[2]))/(1-result[0])
 
-      purity5GeVUp   = purity(5, signalUp,   backgroundUp)
-      purity5GeVDown = purity(5, signalDown, backgroundDown)
-      purity1GeVUp   = purity(1, signalUp,   backgroundUp)
-      purity1GeVDown = purity(1, signalDown, backgroundDown)
+#
+# ROOT file for a signal regions fit
+#
+def writeRootFile(name, systematics, nonPromptSF):
+  try:    os.makedirs('combine')
+  except: pass
 
-      log.info('Result purity 5GeV: %.2f %.2f/+%.2f' % (purity5GeV, purity5GeVDown-purity5GeV, purity5GeV-purity5GeVUp))
-      log.info('Result purity 1GeV: %.2f %.2f/+%.2f' % (purity1GeV, purity1GeVDown-purity1GeV, purity1GeV-purity1GeVUp))
+  f = ROOT.TFile('combine/' + name + '.root', 'RECREATE')
 
-  if args.checkStability:
-    c     = ROOT.TCanvas(channel, channel)
-    graph = ROOT.TGraphAsymmErrors()
-    graph.SetTitle(';MC truth photon purity;Fit result photon purity')
-    for i, (mcPurity, fitPurity) in enumerate(mcPurities):
-      graph.SetPoint(i, mcPurity, fitPurity[0])
-      graph.SetPointError(i, 0, 0, -fitPurity[1], fitPurity[2])
-    graph.GetXaxis().SetLimits(0.31,0.9)
-    graph.SetMinimum(0.31)
-    graph.SetMaximum(0.9)
-    graph.Draw("AP0")
-    line = ROOT.TLine(0.31,0.31,0.9,0.9);
-    line.Draw()
-    c.Print('./fitPlots/' + channel + '/purityComparison.pdf')
-    c.Print('./fitPlots/' + channel + '/purityComparison.png')
-log.info('Finished')
+  baseSelection = 'llg-looseLeptonVeto-mll40-offZ-llgNoZ-photonPt20'
+  writeHist(f, 'sr_OF', 'data_obs', getHistFromPkl(('eleSusyLoose-phoCBfull', 'emu', baseSelection), 'signalRegions', '', ['MuonEG']))
+  writeHist(f, 'sr_SF', 'data_obs', getHistFromPkl(('eleSusyLoose-phoCBfull', 'SF',  baseSelection), 'signalRegions', '', ['DoubleEG'],['DoubleMuon']))
+
+  statVariations = []
+  for sample in samples:
+    promptSelectors    = [[sample, '(genuine,misIdEle)']]
+    nonPromptSelectors = [[sample, '(hadronicPhoton,hadronicFake)']]
+    for sys in [''] + systematics:
+      for shape, channel in [('sr_OF', 'emu'), ('sr_SF', 'SF')]:
+        prompt    = getHistFromPkl(('eleSusyLoose-phoCBfull-match', channel, baseSelection), 'signalRegions', sys, *promptSelectors)
+        nonPrompt = getHistFromPkl(('eleSusyLoose-phoCBfull-match', channel, baseSelection), 'signalRegions', sys, *nonPromptSelectors)
+        if sample.count('TTJets') and nonPromptSF: nonPrompt = applyNonPromptSF(nonPrompt, nonPromptSF)   # Currently only for TTJets, could in principle do this for all non-prompt contributions
+        total = prompt.Clone()
+        total.Add(nonPrompt)
+
+        writeHist(f, shape+sys, sample, total, (statVariations if sys=='' else None))
+
+  return set(statVariations)
+  f.Close()
+
+
+#
+# Signal regions fit
+#
+cardName    = 'srFit'
+templates   = samples
+extraLines  = [(s + '_norm rateParam * ' + s + '* 1') for s in samples[1:]]
+extraLines += [(s + '_norm param 1.0 0.1')            for s in samples[1:]]
+#extraLines += ['nonPrompt rateParam * *_np 1']
+#extraLines += ['nonPrompt param 1.0 0.2']
+#extraLines += ['* autoMCStats 0'] # does not work
+
+statVariations = writeRootFile(cardName, systematics.keys(), nonPromptSF)
+writeCard(cardName, ['sr_OF', 'sr_SF'], templates, extraLines, systematics.keys(), statVariations, linearSystematics)
+
+result = runFitDiagnostics(cardName, trackParameters = ['TTJets_norm', 'ZG_norm','DY_norm','other_norm','r'], toys=None, statOnly=False)
+runImpacts(cardName)
+#runSignificance(cardName)
+#runSignificance(cardName, expected=True)
