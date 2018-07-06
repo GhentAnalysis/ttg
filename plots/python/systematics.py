59fa0514e827c1f37866b1a1ff3675429d3bad4e
UAEDF-tomc <tom.cornelis@ua.ac.be>
2018-07-06 17:54:46 +0200

    Split up in pdfTTGamma and pdfTTbar, do not use the 'Shape' variants for q2 and pdf, the original were actually the correct shape sys

--- plots/python/systematics.py
+++ plots/python/systematics.py
@@ -3,42 +3,48 @@
 #
 systematics = {}
 for i in ('Up', 'Down'):
-  systematics['isr'+i]      = []
-  systematics['fsr'+i]      = []
-  systematics['eScale'+i]   = []
-  systematics['eRes'+i]     = []
-  systematics['phScale'+i]  = []
-  systematics['phRes'+i]    = []
-  systematics['pu'+i]       = [('puWeight',      'puWeight'+i)]
-  systematics['phSF'+i]     = [('phWeight',      'phWeight'+i)]
-  systematics['lSF'+i]      = [('lWeight',       'lWeight'+i)]
-  systematics['trigger'+i]  = [('triggerWeight', 'triggerWeight'+i)]
-  systematics['bTagl'+i]    = [('bTagWeight',    'bTagWeightl'+i)]
-  systematics['bTagb'+i]    = [('bTagWeight',    'bTagWeightb'+i)]
-  systematics['pdf'+i]      = [('genWeight',     'weight_pdfShape'+i)]
-  systematics['q2'+i]       = [('genWeight',     'weight_q2Shape'+i)]
-  systematics['JEC'+i]      = [('njets',         'njets_JEC'+i),     ('ndbjets',    'ndbjets_JEC'+i), ('j1', 'j1_JEC'+i), ('j2', 'j2_JEC'+i), ('_jetPt', '_jetPt_JEC'+i)]
-  systematics['JER'+i]      = [('njets',         'njets_JER'+i),     ('ndbjets',    'ndbjets_JER'+i), ('j1', 'j1_JER'+i), ('j2', 'j2_JER'+i), ('_jetPt', '_jetPt_JER'+i)]
+  systematics['isr'+i]        = []
+  systematics['fsr'+i]        = []
+  systematics['eScale'+i]     = []
+  systematics['eRes'+i]       = []
+  systematics['phScale'+i]    = []
+  systematics['phRes'+i]      = []
+  systematics['pu'+i]         = [('puWeight',      'puWeight'+i)]
+  systematics['phSF'+i]       = [('phWeight',      'phWeight'+i)]
+  systematics['lSF'+i]        = [('lWeight',       'lWeight'+i)]
+  systematics['trigger'+i]    = [('triggerWeight', 'triggerWeight'+i)]
+  systematics['bTagl'+i]      = [('bTagWeight',    'bTagWeightl'+i)]
+  systematics['bTagb'+i]      = [('bTagWeight',    'bTagWeightb'+i)]
+  systematics['pdfTTGamma'+i] = [('genWeight',     'TTGamma:weight_pdf'+i)]
+  systematics['pdfTTbar'+i]   = [('genWeight',     'TTJets_pow:weight_pdf'+i)]
+  systematics['q2'+i]         = [('genWeight',     'weight_q2'+i)]
+  systematics['JEC'+i]        = [('njets',         'njets_JEC'+i),     ('ndbjets',    'ndbjets_JEC'+i), ('j1', 'j1_JEC'+i), ('j2', 'j2_JEC'+i), ('_jetPt', '_jetPt_JEC'+i)]
+  systematics['JER'+i]        = [('njets',         'njets_JER'+i),     ('ndbjets',    'ndbjets_JER'+i), ('j1', 'j1_JER'+i), ('j2', 'j2_JER'+i), ('_jetPt', '_jetPt_JER'+i)]
 
 
 #
 # Defining linear systematics as "name : (sampleList, %)"
 #
 linearSystematics = {}
-linearSystematics['lumi'] = (None, 2.5)
-#linearSystematics['TTGamma'] = (['TTGamma'], 2.5)]
+linearSystematics['lumi']       = (None, 2.5)
+#linearSystematics['TTbar_norm'] = (['TTbar'], 5.5)]
 
 
 #
 # Function to apply the systematic to the cutstring or tree
+# When ':' is used in the sysVar, the first part selects a specific sample on which the sysVar should be applied
 #
-def applySysToString(sys, string):
-  for i in systematics[sys]:
-    var, sysVar = i
+def applySysToString(sample, sys, string):
+  for var, sysVar in systematics[sys]:
+    if sysVar.count(':'):
+      s, sysVar = sysVar.split(':')
+      if sample!=s: return string
     string = string.replace(var, sysVar)
   return string
 
-def applySysToTree(sys, tree):
-  for i in systematics[sys]:
-    var, sysVar = i
+def applySysToTree(sample, sys, tree):
+  for var, sysVar in systematics[sys]:
+    if sysVar.count(':'):
+      s, sysVar = sysVar.split(':')
+      if sample!=s: return
     setattr(tree, var, getattr(tree, sysVar))
