e057971a93a1cfa5970baf03620845a77a856eee
tomc <tomc@cern.ch>
2018-08-12 18:59:00 +0200

    Muon SF calculated with sys

--- reduceTuple/python/leptonSF.py
+++ reduceTuple/python/leptonSF.py
@@ -11,11 +11,8 @@ from ttg.tools.helpers import getObjFromFile
 from math import sqrt
 
 dataDir  = "$CMSSW_BASE/src/ttg/reduceTuple/data/leptonSFData"
-keys_mu  = [("TnP_NUM_MediumID_DENOM_generalTracks_VAR_map_pt_eta.root", "SF"),
-            ("TnP_NUM_TightIP2D_DENOM_MediumID_VAR_map_pt_eta.root",     "SF"),
-            ("TnP_NUM_TightIP3D_DENOM_MediumID_VAR_map_pt_eta.root",     "SF"),
-            ("ratio_NUM_RelIsoVTight_DENOM_MediumID_VAR_map_pt_eta_v2.root","pt_abseta_ratio")]
-keys_ele = [("scaleFactors.root", "GsfElectronToTTG")] # Includes everything, also emulation and isolation cuts
+keys_mu  = [("scaleFactorsMuons.root",     "MuonToTTGamma")]    # Includes both id and iso
+keys_ele = [("scaleFactorsElectrons.root", "GsfElectronToTTG")] # Includes everything, also emulation and isolation cuts
 
 class leptonSF:
   def __init__(self):
@@ -39,13 +36,12 @@ class leptonSF:
     eta    = abs(tree._lEta[index])
 
     if abs(flavor)==1:   
-      if pt >= 120: pt = 119 # last bin is valid to infinity
+      if pt >= 200: pt = 199 # last bin is valid to infinity
       sf = self.mult([self.getPartialSF(effMap, pt, eta) for effMap in self.mu])
-      sf.sigma = 0.03 # Recommendation for Moriond17
     elif abs(flavor)==0:
       if pt >= 200: pt = 199 # last bin is valid to infinity
       sf = self.mult([self.getPartialSF(effMap, pt, eta) for effMap in self.ele])
     else: 
       raise Exception("Lepton SF for flavour %i not known"%flavor)
 
-    return (1+sf.sigma*sigma)*sf.val
+    return (1+(sf.sigma+0.01*sf.val)*sigma)*sf.val                # 1% additional uncertainty to account for phase space differences between Z and ttbar
