462076c9a1d8c43033bb02df0c89d1fbed1b6d61
UAEDF-tomc <tomc@cern.ch>
2018-08-13 17:44:32 +0200

    add TTGamma for sysmappings

--- plots/data/makeSysMappings.py
+++ plots/data/makeSysMappings.py
@@ -7,7 +7,7 @@ with open('sysMappingsTemplate.json', 'r') as template:
         for photonTypeShort, photonType in [('h', 'hadronic photon'), ('g', 'genuine photon'), ('f', 'hadronic fake')]:
           for i in range(1,8):
             mapping.write('  "chgIsoall_' + photonTypeShort + 'Stat' + str(i) + '" : "Stats ' + photonType + ' bin ' + str(i) + '",\n')
-        for sample, sampleTex in [('TTJets', 't#bar{t}'), ('ZG', 'Z#gamma'), ('DY', 'Drell-Yan'), ('other', 'Other')]:
+        for sample, sampleTex in [('TTGamma', 't#bar{t}\gamma'), ('TTJets', 't#bar{t}'), ('ZG', 'Z#gamma'), ('DY', 'Drell-Yan'), ('other', 'Other')]:
           for signs in ['SF', 'OF']:
             for bin, mult in [('1', '1j,1b'), ('2', '#geq2j,0b'), ('3', '#geq2j,1b'), ('4', '#geq2j,#geq2b')]:
               mapping.write('  "sr_' + signs + sample + 'Stat' + bin + '" : "Stats ' + sampleTex + ' ' + mult + ' (' + signs + ')",\n')
