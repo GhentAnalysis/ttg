1397f9a7cfbe7de32a0c4b2a4522616559bd0f90
tomc <tomc@cern.ch>
2018-11-08 14:21:35 +0100

    cleanup

--- plots/python/variations.py
+++ plots/python/variations.py
@@ -26,12 +26,6 @@ onZSelections     = ['llg-looseLeptonVeto-mll40-njet1p-photonPt20',
                      'llg-looseLeptonVeto-mll40-llgOnZ-njet2p-deepbtag2p-photonPt20',
                      ]
 
-qcdSelections     = ['pho-photonPt20',
-                     'pho-njet1p-photonPt20',
-                     'pho-njet2p-photonPt20',
-                     'pho-njet2p-deepbtag1p-photonPt20',
-                     ]
-
 dilepSelections   = ['ll-looseLeptonVeto-mll40',
                      'll-looseLeptonVeto-mll40-nphoton0',
                      'll-looseLeptonVeto-mll40-offZ',
@@ -42,25 +36,15 @@ dilepSelections   = ['ll-looseLeptonVeto-mll40',
                      'll-looseLeptonVeto-mll40-offZ-njet2p-deepbtag1p-nphoton0'
                      ]
 
-silepSelections   = ['lg-looseLeptonVeto-photonPt20',
-                     'lg-looseLeptonVeto-njet3p-photonPt20',
-                     'lg-looseLeptonVeto-njet4p-photonPt20',
-                     'lg-looseLeptonVeto-njet4p-deepbtag1p-photonPt20',
-                     'lg-looseLeptonVeto-njet4p-deepbtag2p-photonPt20']
-
 def getVariations(args, sysList):
   if args.selection:                                                 selections = [args.selection]
-  elif args.tag.count('QCD'):                                        selections = qcdSelections
-  elif args.tag.count('singleLep'):                                  selections = silepSelections
   elif not args.tag.count('pho') and args.tag.count('eleSusyLoose'): selections = dilepSelections
   elif args.tag.count('phoCBfull'):                                  selections = defaultSelections+onZSelections
   else:                                                              selections = defaultSelections
 
   if args.channel:                        channels = [args.channel]
   elif args.tag.count('compareChannels'): channels = ['all']
-  elif args.tag.count('QCD'):             channels = ['noData']
   elif args.tag.count('splitOverlay'):    channels = ['noData']
-  elif args.tag.count('singleLep'):       channels = ['e','mu','noData']
   elif args.tag.count('randomConeCheck'): channels = ['ee','mumu','emu','SF','all']
   elif args.tag.count('igmaIetaIeta'):    channels = ['ee','mumu','emu','SF','all']
   else:                                   channels = ['ee','mumu','emu','SF','all','noData']
