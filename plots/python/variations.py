8068bb29ccdb6f288dc28981563bb1c9d67251dd
tomc <tomc@cern.ch>
2018-11-09 15:05:26 +0100

    Try to reduce the selections probed for systematic and post-fit plots

--- plots/python/variations.py
+++ plots/python/variations.py
@@ -1,6 +1,6 @@
 defaultSelections = ['llg-looseLeptonVeto-mll40-photonPt20',
-                     'llg-looseLeptonVeto-mll40-offZ-llgNoZ-photonPt20',
-                     'llg-looseLeptonVeto-mll40-offZ-llgNoZ-signalRegion-photonPt20',
+                     'llg-looseLeptonVeto-mll40-offZ-llgNoZ-photonPt20:SYS,POST',
+                     'llg-looseLeptonVeto-mll40-offZ-llgNoZ-signalRegion-photonPt20:SYS,POST',
                      'llg-looseLeptonVeto-mll40-offZ-llgNoZ-njet1-deepbtag0-photonPt20',
                      'llg-looseLeptonVeto-mll40-offZ-llgNoZ-njet1-deepbtag1p-photonPt20',
                      'llg-looseLeptonVeto-mll40-offZ-llgNoZ-njet2p-photonPt20',
@@ -11,36 +11,49 @@ defaultSelections = ['llg-looseLeptonVeto-mll40-photonPt20',
                      ]
 
 onZSelections     = ['llg-looseLeptonVeto-mll40-njet1p-photonPt20',
-                     'llg-looseLeptonVeto-mll40-signalRegion-photonPt20',
+                     'llg-looseLeptonVeto-mll40-signalRegion-photonPt20:SYS',
                      'llg-looseLeptonVeto-mll40-orOnZ-photonPt20',
                      'llg-looseLeptonVeto-mll40-onZ-photonPt20',
-                     'llg-looseLeptonVeto-mll40-llgOnZ-photonPt20',
-                     'llg-looseLeptonVeto-mll40-llgOnZ-signalRegion-photonPt20',
-                     'llg-looseLeptonVeto-mll40-llgOnZ-njet1p-photonPt20',
-                     'llg-looseLeptonVeto-mll40-llgOnZ-njet1-deepbtag0-photonPt20',
-                     'llg-looseLeptonVeto-mll40-llgOnZ-njet1-deepbtag1p-photonPt20',
-                     'llg-looseLeptonVeto-mll40-llgOnZ-njet2p-photonPt20',
-                     'llg-looseLeptonVeto-mll40-llgOnZ-njet2p-deepbtag0-photonPt20',
-                     'llg-looseLeptonVeto-mll40-llgOnZ-njet2p-deepbtag1-photonPt20',
-                     'llg-looseLeptonVeto-mll40-llgOnZ-njet2p-deepbtag1p-photonPt20',
-                     'llg-looseLeptonVeto-mll40-llgOnZ-njet2p-deepbtag2p-photonPt20',
+                     'llg-looseLeptonVeto-mll40-llgOnZ-photonPt20:SYS,POST',
+                     'llg-looseLeptonVeto-mll40-llgOnZ-signalRegion-photonPt20:SYS,POST',
                      ]
 
-dilepSelections   = ['ll-looseLeptonVeto-mll40',
-                     'll-looseLeptonVeto-mll40-nphoton0',
-                     'll-looseLeptonVeto-mll40-offZ',
-                     'll-looseLeptonVeto-mll40-offZ-nphoton0',
+diffSelections    = ['llg-looseLeptonVeto-mll40-offZ-llgNoZ-signalRegion-photonPt20to30',
+                     'llg-looseLeptonVeto-mll40-offZ-llgNoZ-signalRegion-photonPt30to50',
+                     'llg-looseLeptonVeto-mll40-offZ-llgNoZ-signalRegion-photonPt50',
+                     'llg-looseLeptonVeto-mll40-offZ-llgNoZ-signalRegion-photonPt50',
+                     'llg-looseLeptonVeto-mll40-offZ-llgNoZ-signalRegion-phJetDeltaR0to0.4',
+                     'llg-looseLeptonVeto-mll40-offZ-llgNoZ-signalRegion-phJetDeltaR0.4to1',
+                     'llg-looseLeptonVeto-mll40-offZ-llgNoZ-signalRegion-phJetDeltaR1',
+                     'llg-looseLeptonVeto-mll40-offZ-llgNoZ-signalRegion-phLepDeltaR0to0.4',
+                     'llg-looseLeptonVeto-mll40-offZ-llgNoZ-signalRegion-phLepDeltaR0.4to1',
+                     'llg-looseLeptonVeto-mll40-offZ-llgNoZ-signalRegion-phLepDeltaR1',
+                     'llg-looseLeptonVeto-mll40-llgOnZ-signalRegion-photonPt20to30',
+                     'llg-looseLeptonVeto-mll40-llgOnZ-signalRegion-photonPt30to50',
+                     'llg-looseLeptonVeto-mll40-llgOnZ-signalRegion-photonPt50',
+                     ]
+
+dilepSelections   = ['ll-looseLeptonVeto-mll40-offZ:SYS',
+                     'll-looseLeptonVeto-mll40-offZ-nphoton0:SYS',
                      'll-looseLeptonVeto-mll40-offZ-njet2p',
                      'll-looseLeptonVeto-mll40-offZ-njet2p-nphoton0',
                      'll-looseLeptonVeto-mll40-offZ-njet2p-deepbtag1p',
                      'll-looseLeptonVeto-mll40-offZ-njet2p-deepbtag1p-nphoton0'
                      ]
 
+
+
 def getVariations(args, sysList):
   if args.selection:                                                 selections = [args.selection]
   elif not args.tag.count('pho') and args.tag.count('eleSusyLoose'): selections = dilepSelections
-  elif args.tag.count('phoCBfull'):                                  selections = defaultSelections+onZSelections
+  elif args.tag=='eleSusyLoose-phoCBfull':                           selections = [(s+':SYS,POST') for s in defaultSelections + onZSelections] + diffSelections
+  elif args.tag.count('phoCBfull'):                                  selections = defaultSelections + onZSelections
   else:                                                              selections = defaultSelections
+
+  # When running sys or post-fit plots, reduce the selection
+  if args.runSys or args.showSys: selections = [s for s in selections if 'SYS'  in s]
+  if args.post:                   selections = [s for s in selections if 'POST' in s]
+  selections = [s.split(':')[0] for s in selections]
 
   if args.channel:                        channels = [args.channel]
   elif args.tag.count('compareChannels'): channels = ['all']
