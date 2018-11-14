dae1f1ed5c812334e6617a6c61629bbf3cd8e9ce
tomc <tomc@cern.ch>
2018-11-14 12:28:25 +0100

    fix

--- plots/getYield.py
+++ plots/getYield.py
@@ -10,5 +10,5 @@ log = getLogger(args.logLevel)
 
 for sample in ['TTGamma', 'TTJets']:
   selection = 'llg-looseLeptonVeto-mll40-offZ-llgNoZ-njet2p-deepbtag1p-photonPt20'
-  print sample + ':\t\t\t'           + str(getHistFromPkl(('eleSusyLoose-phoCBfull',              'all', selection, 'yield', '', [sample])).Integral())
-  print sample + '(prefireCheck):\t' + str(getHistFromPkl(('eleSusyLoose-phoCBfull-prefireCheck', 'all', selection, 'yield', '', [sample])).Integral())
+  print sample + ':\t\t\t'           + str(getHistFromPkl('eleSusyLoose-phoCBfull',              'all', selection, 'yield', '', [sample]).Integral())
+  print sample + '(prefireCheck):\t' + str(getHistFromPkl('eleSusyLoose-phoCBfull-prefireCheck', 'all', selection, 'yield', '', [sample]).Integral())
