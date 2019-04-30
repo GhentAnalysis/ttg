a06203b1315692859d20fabee1027a24b84bc930
tomc <tomc@cern.ch>
2019-04-30 12:40:20 +0200

    tdr notes: make sure utils is always checked out

--- documents/setupPaper.sh
+++ documents/setupPaper.sh
@@ -3,5 +3,8 @@ if [ ! -d "$note" ]; then
   git clone --recursive https://gitlab.cern.ch/tdr/papers/$note.git 
 fi
 cd $note
+if [ ! -d utils ]; then # sometimes there is a connection problem and utils isn't checked out
+  git clone --recursive https://gitlab.cern.ch/tdr/utils.git 
+fi
 eval `./utils/tdr runtime -sh`
 tdr --temp_dir=$PWD/tmp --style=paper b
