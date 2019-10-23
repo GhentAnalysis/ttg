3d0bb8eb24e0bcc148677735dfc7a3ff180b47e0
tomc <tomc@cern.ch>
2019-10-23 18:41:27 +0200

    Ignore the utils directory for the code review

--- tools/codeReview/lint.py
+++ tools/codeReview/lint.py
@@ -5,7 +5,7 @@ import os
 #
 # Runs pylint on the whole ttg package, but exclude dirs and files which are from external sources
 #
-skipDirs  = ['documents', 'log', '.git', 'virenv']
+skipDirs  = ['documents', 'log', '.git', 'virenv', 'utils']
 skipFiles = ['__init__.py', 'diffNuisances.py']
 
 pyFiles = []
---
+++ utils/README.md
@@ -0,0 +1,1 @@
+Directory probably outdated ... might need some clean-up
